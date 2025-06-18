eda2789caedc35425ec56050a3bba8447efb0fb4
ivanstiv <albertduf@mail.ru>
2025-06-18 10:01:51 +0300

    sad

---
+++ test_form.rb
@@ -0,0 +1,10 @@
+require 'ostruct'
+require_relative 'hexlet_code'
+
+user = OpenStruct.new(name: "rob", job: "hexlet")
+
+form_html = HexletCode.form_for(user) do |f|
+  f.input :name, label: "Name", class: "user-input"
+  f.input :job, label: "Job", as: :text
+  f.submit "Wow"
+end
