89abf6b536bfcc2a622d67ad4bde61e03d75fbd7
ivanstiv <albertduf@mail.ru>
2025-06-25 08:25:25 +0300

    sad

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -2,64 +2,89 @@ require_relative "hexlet_code/version"
 require_relative "hexlet_code/tag"
 
 module HexletCode
-  autoload(:FormBuilder, 'hexlet_code/form_builder')
-  autoload(:FormRenderer, 'hexlet_code/form_renderer')
-  autoload(:Tag, 'hexlet_code/tag')
+  def self.form_for(object, url: '#', **attributes, &block)
+    form_attributes = { action: url, method: 'post' }.merge(attributes)
+    builder = TagBuilder.new(object)
+    form_content = block_given? ? yield(builder) : ''
+    builder.form_content = form_content
 
-  def self.form_for(object, attributes = {})
-    builder = FormBuilder.new(object, **attributes)
-    yield(builder) if block_given?
-    FormRenderer.render_html(builder)
+    HexletCode::Tag.build('form', form_attributes) { builder.form_content }
   end
 
-  class FormBuilder
-    attr_reader :form_body
+  class TagBuilder
+    attr_accessor :form_content
 
-    def initialize(object, attributes = {})
+    def initialize(object)
       @object = object
-      @form_body = {
-        inputs: [],
-        submit: { options: nil },
-        form_options: {
-          action: attributes.fetch(:url, '#'),
-          method: attributes.fetch(:method, 'post')
-        }.merge(attributes.except(:url, :method))
-      }
+      @form_content = ''
     end
 
     def input(name, **options)
-      @form_body[:inputs] << { name: name, options: options }
+      value = @object.public_send(name).to_s
+      
+      if options[:as] == :text
+        options.delete(:as)
+        @form_content += HexletCode::Tag.build('textarea', name: name, **options) { value }
+      else
+        options = { type: 'text', name: name, value: value }.merge(options)
+        @form_content += HexletCode::Tag.build('input', options)
+      end
     end
 
-    def submit(value = 'Save', **options)
-      @form_body[:submit] = { options: options.merge(value: value) }
+    def submit(value = 'Save')
+      @form_content += HexletCode::Tag.build('input', type: 'submit', value: value)
     end
   end
 
-  class FormRenderer
-    def self.render_html(builder)
-      form_options = builder.form_body[:form_options]
-      inputs = builder.form_body[:inputs]
-      submit = builder.form_body[:submit]
+  module InputHelper
+    def self.input(prompt = "> ", as: :string)
+      print prompt
+      user_input = gets.chomp
 
-      inputs_html = inputs.map do |input|
-        name = input[:name]
-        options = input[:options] || {}
-        value = builder.object.public_send(name) rescue nil
+      case as
+      when :string
+        user_input
+      when :integer
+        user_input.to_i
+      when :float
+        user_input.to_f
+      when :array
+        user_input.split
+      when :boolean
+        user_input.downcase == "true" || user_input.downcase == "yes"
+      else
+        raise ArgumentError, "Unsupported type: #{as}"
+      end
+    end
 
-        if options[:as] == :text
-          HexletCode::Tag.build('textarea', name: name, **options.except(:as)) { value.to_s }
-        else
-          input_type = options.fetch(:as, 'text')
-          HexletCode::Tag.build('input', type: input_type, name: name, value: value.to_s, **options.except(:as))
-        end
-      end.join
+    class Form
+      def initialize
+        @fields = {}
+        @submitted = false
+      end
 
-      submit_html = HexletCode::Tag.build('input', type: 'submit', **submit[:options])
+      def inputy(name, label: nil, as: :string, **options)
+        label ||= name.to_s.capitalize
+        prompt = "#{label}: "
+        
+        value = InputHelper.input(prompt, as: as, **options)
+        @fields[name] = value
+      end
 
-      HexletCode::Tag.build('form', **form_options) do
-        inputs_html + submit_html
+      def submit
+        @submitted = true
+        @fields
+      end
+
+      def submitted?
+        @submitted
+      end
+
+      def [](name)
+        @fields[name]
       end
     end
   end
+
+  class Error < StandardError; end
 end
